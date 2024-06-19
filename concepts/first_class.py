51381e26b618740def3225690d6677808eef4407
Henrique Torres <henriquetorrescampos@gmail.com>
2024-06-18 21:44:48 -0300

    First Commit

---
+++ concepts/first_class.py
@@ -0,0 +1,9 @@
+class MinhaClasse:
+
+    def metodo_1(self): #self se refere a classe
+        print('My action1')
+
+#objeto          #classe instancia objeto
+minha_classe = MinhaClasse()
+
+minha_classe.metodo_1()
